d364503c318c5ac1bfe326b319541336a36adf80
ugurrApak <101298562+ugurrApak@users.noreply.github.com>
2022-05-28 05:24:05 +0300

    FixBug

--- Jamination/Assets/Scripts/CharacterMovement.cs
+++ Jamination/Assets/Scripts/CharacterMovement.cs
@@ -8,11 +8,13 @@ public class CharacterMovement : MonoBehaviour
     float horizontalMove;
     float verticalMove;
     bool isGrounded = true;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         playerTransform = GetComponent<Transform>();
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0F;
     }
 
     // Update is called once per frame
@@ -29,16 +31,17 @@ public class CharacterMovement : MonoBehaviour
             playerTransform.position += new Vector3(0f,verticalMove,0f);
         }
         
-        if(!isGrounded)
-        {
-            playerTransform.localScale -= new Vector3(0.2f,0.2f,0f);
-        }
+        // if(!isGrounded && playerTransform.localScale.x > 0f && playerTransform.localScale.y > 0)
+        // {
+        //     playerTransform.localScale += new Vector3(-0.2f * Time.deltaTime,-0.2f * Time.deltaTime ,0f);
+        // }
     }
     void OnTriggerExit2D(Collider2D other)
     {
         if(other.gameObject.tag == "Ground")
         {
             isGrounded = false;
+            Debug.Log("Exit");
         }
     }
     void OnTriggerStay2D(Collider2D other)
@@ -46,6 +49,7 @@ public class CharacterMovement : MonoBehaviour
         if(other.gameObject.tag == "Ground")
         {
             isGrounded = true;
+            Debug.Log("Enter");
         }
     }
 }
