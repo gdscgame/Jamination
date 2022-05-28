66be682e8fd6a995580d408f5d3a2a31e163b7f0
eskin04 <gdscgameteam@gmail.com>
2022-05-28 04:17:24 +0300

    Revert "Add Movement"
    This reverts commit 6b1e694a0ce456ad5bc135b2b3e6de93b8d704ab.

--- Jamination/Assets/Scripts/CharacterMovement.cs
+++ Jamination/Assets/Scripts/CharacterMovement.cs
@@ -4,47 +4,15 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    Transform playerTransform;
-    float horizontalMove;
-    float verticalMove;
-    bool isGrounded = true;
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-        playerTransform = GetComponent<Transform>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal");
-        verticalMove = Input.GetAxisRaw("Vertical");
-        if(Input.GetButtonDown("Horizontal") && isGrounded)
-        {
-            playerTransform.position += new Vector3(horizontalMove,0f,0f);
-        }
-        else if(Input.GetButtonDown("Vertical") && isGrounded)
-        {
-            playerTransform.position += new Vector3(0f,verticalMove,0f);
-        }
         
-        if(!isGrounded)
-        {
-            playerTransform.localScale -= new Vector3(0.2f,0.2f,0f);
-        }
-    }
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if(other.gameObject.tag == "Ground")
-        {
-            isGrounded = false;
-        }
-    }
-    void OnTriggerStay2D(Collider2D other)
-    {
-        if(other.gameObject.tag == "Ground")
-        {
-            isGrounded = true;
-        }
     }
 }
